583fb31001cb9a1224ab00aeaeb4336b73d472f9
vitaliy <tolikumc@gmail.com>
2019-10-15 21:10:50 +0300

    useEffect

--- src/components/profile-page/profile-info/status/index.js
+++ src/components/profile-page/profile-info/status/index.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const Status = props => {
   const [editMode, setMode] = useState(false);
   const [status, setStatus] = useState(props.status);
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
   return (
     <>
       {editMode ? (
